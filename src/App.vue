7e659ba1866dc8dff0984751ba606a0a80a02a26
Mehdi Mortazavian <you@example.com>
2022-07-09 18:46:23 +0430

    add extra css and change the style of button

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,8 @@
   </div>
   <!-- <nav> -->
   <!-- <router-link to="/">Home</router-link> -->
-  <router-link to="/login">Log in</router-link>
-  <router-link to="/sign-up">Sign Up</router-link>
+  <!-- <router-link to="/login">Log in</router-link>
+  <router-link to="/sign-up">Sign Up</router-link> -->
   <!-- </nav> -->
 </template>
 
